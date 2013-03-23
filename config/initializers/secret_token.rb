ee1bfbc2e73a5edc5c4af5aa4621e20bd9782dc3
Anthony Erlinger <aerlinger@gmail.com>
2013-03-23 18:15:56 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NavDatavbase::Application.config.secret_token = '717dd94e699c13992089440b883be4be1c6da589633af56406323b921ca9ed2803238e79584ad3f500655fd79d7d0943bf2d98f3a2807f9a2429ec7c756b6c7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NavDatavbase::Application.config.session_store :cookie_store, key: '_navDatavbase_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NavDatavbase::Application.config.session_store :active_record_store
